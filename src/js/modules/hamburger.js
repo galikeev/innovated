005f27d4d2fde96c2b808954cce8829c65930ef8
Ramil <vk2011@bk.ru>
2021-09-05 15:13:47 +0500

    add animation for hamburger menu

--- src/js/modules/hamburger.js
+++ src/js/modules/hamburger.js
@@ -3,28 +3,33 @@ const hamburger = () => {
     const menu = document.querySelector('.menu'),
         menuLinks = document.querySelectorAll('.hamburger__links'),
         hamburger = document.querySelector('.hamburger'),
-        bodyScroll = document.querySelector('body');
+        hamburgerHeader = document.querySelector('.hamburger__wrapper');
 
 
     menu.addEventListener('click', () => {
         menu.classList.toggle('menu_active');
         hamburger.classList.toggle('hamburger_active');
-        bodyScroll.classList.add('menu_scroll');
+        hamburgerHeader.classList.remove('animate__animated', 'animate__fadeOutRight');
+        hamburgerHeader.classList.add('animate__animated', 'animate__fadeInRight');
+        setTimeout(() => {
+            hamburgerHeader.classList.remove('animate__animated', 'animate__fadeInRight');
+        }, 1000);
     });
 
     menuLinks.forEach(item => {
         item.addEventListener('click', () => {
             menu.classList.toggle('menu_active');
             hamburger.classList.toggle('hamburger_active');
-            bodyScroll.classList.remove('menu_scroll');
+            hamburgerHeader.classList.remove('animate__animated', 'animate__fadeInRight');
         });
     });
+    
 
     hamburger.addEventListener('click', (e) => {
         if (e.target === hamburger) {
             menu.classList.toggle('menu_active');
             hamburger.classList.toggle('hamburger_active');
-            bodyScroll.classList.remove('menu_scroll');
+            hamburgerHeader.classList.remove('animate__animated', 'animate__fadeInRight');
         }
     });
 
